5a438fc2cb1b470cb013f6ae1c355637f4d1cee3
Ahmed <ahmed.mamdouh2495@gmail.com>
2017-11-13 19:28:52 +0200

    initial commit

---
+++ Mini_Project_2/Assets/Scripts/EthanMove.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EthanMove : MonoBehaviour {
+
+	Animator anim;
+
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+
+	void Update () {
+
+		// Moving forward
+		if (Input.GetAxis ("Vertical") > 0) {
+			anim.SetFloat ("walkingSpeed", 1);
+			anim.SetBool ("movingForward", true);
+		}
+
+		else if (Input.GetAxis ("Vertical") < 0) {
+			anim.SetFloat ("walkingSpeed", -1);
+			anim.SetBool ("movingForward", true);
+		}
+
+		else if (Input.GetAxis ("Vertical") == 0) {
+			anim.SetFloat ("walkingSpeed", 1);
+			anim.SetBool ("movingForward", false);
+		}
+
+		// Turning
+		if (Input.GetAxis ("Horizontal") > 0) {
+			anim.SetBool ("turningRight", true);
+		} 
+		else if (Input.GetAxis ("Horizontal") < 0) {
+			anim.SetBool ("turningLeft", true);
+		} 
+		else {
+			anim.SetBool ("turningLeft", false);
+			anim.SetBool ("turningRight", false);
+		}
+
+	}
+}
